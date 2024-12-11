8c63b38c496cbd3ca4c72b910a841c400321b9c3
DutraGames <gabrieldutraamarante@gmail.com>
2024-12-11 09:22:13 -0300

    feat: types Developer and Game

---
+++ src/@types/typesDeveloper.ts
@@ -0,0 +1,9 @@
+export type Developer = {
+    id: string;
+    name: string;
+    email: string;
+    website: string;
+    twitter: string;
+    instagram: string;
+    about: string;
+};
---
+++ src/@types/typesGame.ts
@@ -0,0 +1,11 @@
+export type Game = {
+  id: string;
+  name: string;
+  bannerUrl: string;
+  description: String;
+  xbox: String;
+  psn: String;
+  nintendo: String;
+  steam: String;
+  discord: String;
+};
