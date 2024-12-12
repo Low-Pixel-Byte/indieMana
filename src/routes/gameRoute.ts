80621b5017b1a6544839e7af4cf02a5ef372541e
DutraGames <gabrieldutraamarante@gmail.com>
2024-12-12 20:05:31 -0300

    feat: add route Game

---
+++ src/routes/gameRoute.ts
@@ -0,0 +1,39 @@
+import { FastifyInstance } from "fastify";
+import { GameCreate } from "../@types/typesGame";
+import { GameUseCase } from "../usecases/GameUseCase";
+
+export async function GameRoute(fastify: FastifyInstance) {
+  fastify.post<{ Body: GameCreate }>("/", async (request, reply) => {
+    const gameUseCase = new GameUseCase();
+    try {
+      const {
+        name,
+        bannerUrl,
+        description,
+        xbox,
+        psn,
+        nintendo,
+        steam,
+        discord,
+        developerId,
+      } = request.body;
+
+      const game = await gameUseCase.create({
+        name,
+        bannerUrl,
+        description,
+        xbox,
+        psn,
+        nintendo,
+        steam,
+        discord,
+        developerId,
+      });
+
+      return reply.status(201).send(game);
+    } catch (error) {
+      console.log(error);
+      return reply.status(500).send({ message: error });
+    }
+  });
+}
