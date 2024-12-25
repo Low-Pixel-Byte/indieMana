f1d5e176010a83762631b937af5b584ea4f8908a
DutraGames <gabrieldutraamarante@gmail.com>
2024-12-25 16:46:21 -0300

    refactor: add layer for futures fetures

--- front-end/src/components/Header/index.tsx
+++ front-end/src/components/Header/index.tsx
@@ -2,46 +2,54 @@ import React from "react";
 
 export default function Header() {
   return (
-    <header className="flex items-center justify-between p-5 bg-indigo-700">
-      <h1 className="text-3xl font-bold text-white">IndieMana</h1>
-      <nav>
-        <ul className="flex items-center gap-4">
-          <li className="">
-            <a className="text-white hover:text-gray-400 text-lg" href="/games">
-              Jogos
-            </a>
-          </li>
-          <li>
-            <a
-              className="text-white hover:text-gray-400 text-lg"
-              href="/developers"
-            >
-              Desenvolvedores
-            </a>
-          </li>
-          <li>
-            <a
-              className="text-white hover:text-gray-400 text-lg"
-              href="/community"
-            >
-              Comunidade
-            </a>
-          </li>
-          <li>
-            <a className="text-white hover:text-gray-400 text-lg" href="/about">
-              Sobre
-            </a>
-          </li>
-        </ul>
-      </nav>
+    <header className="flex p-5 bg-indigo-700">
+      <div className="container flex items-center mx-auto justify-between">
+        <h1 className="text-3xl font-bold text-white">IndieMana</h1>
+        <nav>
+          <ul className="flex items-center gap-4">
+            <li className="">
+              <a
+                className="text-white hover:text-gray-400 text-lg"
+                href="/games"
+              >
+                Jogos
+              </a>
+            </li>
+            <li>
+              <a
+                className="text-white hover:text-gray-400 text-lg"
+                href="/developers"
+              >
+                Desenvolvedores
+              </a>
+            </li>
+            <li>
+              <a
+                className="text-white hover:text-gray-400 text-lg"
+                href="/community"
+              >
+                Comunidade
+              </a>
+            </li>
+            <li>
+              <a
+                className="text-white hover:text-gray-400 text-lg"
+                href="/about"
+              >
+                Sobre
+              </a>
+            </li>
+          </ul>
+        </nav>
 
-      <div className="flex items-center">
-        <input type="text" />
+        <div className="flex items-center">
+          <input type="text" />
+        </div>
+
+        <button className="px-4 py-2 text-black bg-yellow-300 rounded text-lg font-semibold">
+          Sugerir jogo
+        </button>
       </div>
-
-      <button className="px-4 py-2 text-black bg-yellow-300 rounded text-lg font-semibold">
-        Sugerir jogo
-      </button>
     </header>
   );
 }
