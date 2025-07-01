2421867ea4e0138017ad69602699cf0885cb3a50
DutraGames <gabrieldutraamarante@gmail.com>
2025-07-01 10:40:25 -0300

    chore: create nestjs project

--- back-end/src/interfaces/ICategory.ts
+++
@@ -1,7 +0,0 @@
-import { Category, CategoryCreate } from "../@types/typesCategory";
-
-export interface ICategory {
-  create(category: CategoryCreate): Promise<Category>;
-  findAll(): Promise<Category[]>;
-  findById(id: number): Promise<Category | null>;
-}
