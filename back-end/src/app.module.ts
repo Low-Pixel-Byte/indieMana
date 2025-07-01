533b96fb25ce9ac1d159f6e245b9c0f8fc66d580
DutraGames <gabrieldutraamarante@gmail.com>
2025-07-01 14:07:43 -0300

    refactor: add res games

--- back-end/src/app.module.ts
+++ back-end/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CategoryModule } from './category/category.module';
+import { GamesModule } from './games/games.module';
 
 @Module({
-  imports: [CategoryModule],
+  imports: [CategoryModule, GamesModule],
   controllers: [AppController],
   providers: [AppService],
 })
