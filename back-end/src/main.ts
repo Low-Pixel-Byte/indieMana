734bfff98fe014a59b675b4c61ee29c543bb5a9f
DutraGames <gabrieldutraamarante@gmail.com>
2025-07-01 11:02:58 -0300

    refactor: add swagger

--- back-end/src/main.ts
+++ back-end/src/main.ts
@@ -1,8 +1,50 @@
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, OpenAPIObject, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
+import { fastifyCors } from '@fastify/cors';
+import * as dotenv from 'dotenv';
+import { RawServerDefault } from 'fastify';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+dotenv.config();
+
+async function bootstrap(): Promise<void> {
+  const app: NestFastifyApplication<RawServerDefault> =
+    await NestFactory.create<NestFastifyApplication>(
+      AppModule,
+      new FastifyAdapter(),
+    );
+
+  await app.register(fastifyCors, {
+    origin: '*',
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+  });
+
+  // Configuration Swagger
+  const configSwagger: Omit<OpenAPIObject, 'paths'> = new DocumentBuilder()
+    .setTitle('IndieMana API')
+    .setDescription('Plataform for indie games')
+    .setVersion('1.0')
+    .build();
+
+  // Configuration document
+  const document: OpenAPIObject = SwaggerModule.createDocument(
+    app,
+    configSwagger,
+  );
+
+  // Configuration swagger endpoint
+  SwaggerModule.setup('api', app, document);
+
+  // Configuração do pipe de validação de dados
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
+
+  // Inicialização do servidor Nest
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
