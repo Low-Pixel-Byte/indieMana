903b7ac0e58f8708dca3191cc739ce97a143582c
DutraGames <gabrieldutraamarante@gmail.com>
2025-07-02 15:43:45 -0300

    test: add tests initials in category controller

---
+++ back-end/src/category/category.controller.spec.ts
@@ -0,0 +1,56 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { CategoryController } from './category.controller';
+import { randomInt } from 'crypto';
+import { CreateCategoryDto } from './dto/create-category.dto';
+import { CategoryService } from './category.service';
+import { PrismaService } from '../prisma.service';
+import { BadRequestException } from '@nestjs/common';
+
+describe('CategoryController', () => {
+  let controller: CategoryController;
+  let prismaService: PrismaService;
+  let categoryService: CategoryService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [CategoryController],
+      providers: [CategoryService, PrismaService],
+    }).compile();
+
+    controller = module.get<CategoryController>(CategoryController);
+    prismaService = module.get<PrismaService>(PrismaService);
+    categoryService = module.get<CategoryService>(CategoryService);
+  });
+
+  it('should be defined', () => {
+    expect(controller).toBeDefined();
+  });
+
+  it('should create category', async () => {
+    const createCategoryDto: CreateCategoryDto = {
+      name: 'Indie',
+    };
+
+    categoryService.create = jest.fn().mockResolvedValueOnce(createCategoryDto);
+
+    const result = await controller.create(createCategoryDto);
+
+    expect(result).toBeDefined();
+  });
+
+  it('should throw BadRequestException when category name is null', async () => {
+    const createCategoryDto: CreateCategoryDto = {
+      name: '',
+    };
+
+    await expect(controller.create(createCategoryDto)).rejects.toThrow(
+      BadRequestException,
+    );
+  });
+
+  it('should badRequestException when id is null', async () => {
+    const id = '';
+
+    await expect(controller.findOne(id)).rejects.toThrow(BadRequestException);
+  });
+});
