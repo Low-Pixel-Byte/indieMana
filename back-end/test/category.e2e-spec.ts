ead73fc02029b5467d66323af06e6bcde4e6da43
DutraGames <gabrieldutraamarante@gmail.com>
2025-07-03 14:26:33 -0300

    test: write test for adapter Fastify

--- back-end/test/category.e2e-spec.ts
+++ back-end/test/category.e2e-spec.ts
@@ -1,13 +1,16 @@
 import { Test } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { CategoryModule } from '../src/category/category.module';
 import { CategoryService } from '../src/category/category.service';
 import { PrismaService } from '../src/prisma.service';
 import { randomInt } from 'crypto';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 
 describe('CategoryController (e2e)', () => {
-  let app: INestApplication;
+  let app: NestFastifyApplication;
   let categoryService: CategoryService;
 
   const createCategoryDto = {
@@ -20,49 +23,82 @@ describe('CategoryController (e2e)', () => {
       providers: [CategoryService, PrismaService],
     }).compile();
 
-    app = moduleRef.createNestApplication();
+    app = moduleRef.createNestApplication<NestFastifyApplication>(
+      new FastifyAdapter(),
+    );
 
     categoryService = moduleRef.get<CategoryService>(CategoryService);
 
     await app.init();
+    await app.getHttpAdapter().getInstance().ready();
   });
 
   it('/category (POST) should create category', async () => {
-    return request(app.getHttpServer())
-      .post('/category')
-      .send(createCategoryDto)
-      .expect(201);
+    return app
+      .inject({
+        method: 'POST',
+        url: '/category',
+        payload: createCategoryDto,
+      })
+      .then((res) => {
+        expect(res.statusCode).toEqual(201);
+      });
   });
 
   it('/category (POST) name already exists', async () => {
-    return request(app.getHttpServer())
-      .post('/category')
-      .send(createCategoryDto)
-      .expect(409);
+    return app
+      .inject({
+        method: 'POST',
+        url: '/category',
+        payload: createCategoryDto,
+      })
+      .then((res) => {
+        expect(res.statusCode).toEqual(409);
+      });
   });
 
   it('/category (GET) should return all categories', async () => {
-    return request(app.getHttpServer()).get('/category').expect(200);
+    return app
+      .inject({
+        method: 'GET',
+        url: '/category',
+      })
+      .then((res) => {
+        expect(res.statusCode).toEqual(200);
+      });
   });
 
   it('/category/:id (GET) should return one category', async () => {
     const categories = await categoryService.findAll();
 
-    return request(app.getHttpServer())
-      .get(`/category/${categories[0].id}`)
-      .expect(200);
+    return app
+      .inject({
+        method: 'GET',
+        url: `/category/${categories[0].id}`,
+      })
+      .then((res) => {
+        expect(res.statusCode).toEqual(200);
+      });
   });
 
   it('/category/:id (GET) should return BadRequestException', async () => {
     const id = '';
 
-    return request(app.getHttpServer()).get(`/category/${+id}`).expect(400);
+    return app
+      .inject({ method: 'GET', url: `/category/${+id}` })
+      .then((res) => {
+        expect(res.statusCode).toEqual(400);
+      });
   });
 
   it('/category/:id (GET) should return NotFoundException', async () => {
     const id = randomInt(1000, 10000);
 
-    return request(app.getHttpServer()).get(`/category/${+id}`).expect(404);
+    return app
+      .inject({ method: 'GET', url: `/category/${+id}` })
+      .then((res) => {
+        expect(res.statusCode).toEqual(404);
+      });
   });
 
   it('/category/:id (PATCH) should update category', async () => {
@@ -72,10 +108,15 @@ describe('CategoryController (e2e)', () => {
       name: `Indie-Updated-${Date.now()}`,
     };
 
-    return request(app.getHttpServer())
-      .patch(`/category/${categories[0].id}`)
-      .send(updateCategoryDto)
-      .expect(204);
+    return app
+      .inject({
+        method: 'PATCH',
+        url: `/category/${categories[0].id}`,
+        payload: updateCategoryDto,
+      })
+      .then((res) => {
+        expect(res.statusCode).toEqual(204);
+      });
   });
 
   it('/category/:id (PATCH) should return BadRequestException', async () => {
@@ -85,10 +126,15 @@ describe('CategoryController (e2e)', () => {
       name: `Indie-Updated-${Date.now()}`,
     };
 
-    return request(app.getHttpServer())
-      .patch(`/category/${+id}`)
-      .send(updateCategoryDto)
-      .expect(400);
+    return app
+      .inject({
+        method: 'PATCH',
+        url: `/category/${+id}`,
+        payload: updateCategoryDto,
+      })
+      .then((res) => {
+        expect(res.statusCode).toEqual(400);
+      });
   });
 
   it('/category/:id (PATCH) should return NotFoundException', async () => {
@@ -98,10 +144,15 @@ describe('CategoryController (e2e)', () => {
       name: `Indie-Updated-${Date.now()}`,
     };
 
-    return request(app.getHttpServer())
-      .patch(`/category/${+id}`)
-      .send(updateCategoryDto)
-      .expect(404);
+    return app
+      .inject({
+        method: 'PATCH',
+        url: `/category/${+id}`,
+        payload: updateCategoryDto,
+      })
+      .then((res) => {
+        expect(res.statusCode).toEqual(404);
+      });
   });
 
   it('/category/:id (DELETE) should delete category', async () => {
@@ -115,12 +166,20 @@ describe('CategoryController (e2e)', () => {
   it('/category/:id (DELETE) should return BadRequestException', async () => {
     const id = '';
 
-    return request(app.getHttpServer()).delete(`/category/${+id}`).expect(400);
+    return app
+      .inject({ method: 'DELETE', url: `/category/${+id}` })
+      .then((res) => {
+        expect(res.statusCode).toEqual(400);
+      });
   });
 
   it('/category/:id (DELETE) should return NotFoundException', async () => {
     const id = randomInt(1000, 10000);
 
-    return request(app.getHttpServer()).delete(`/category/${+id}`).expect(404);
+    return app
+      .inject({ method: 'DELETE', url: `/category/${+id}` })
+      .then((res) => {
+        expect(res.statusCode).toEqual(404);
+      });
   });
 });
